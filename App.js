a36bbafbbfead9fac89b478cbd2a6f9bac7b699d
Renata Santos <123095086+Re-Santos@users.noreply.github.com>
2023-05-24 16:48:57 -0300

    Exercicios1

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import HomeExercicios from './exercicios/HomeExercicios';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Renata</h1>
+      <HomeExercicios/>
+    
+    
+    </div>
+  );
+}
+
+export default App;
